e66c8f0911786ddaf43224a329d8713e27d374ba
Kishore <35829521+Kishore7298@users.noreply.github.com>
2018-11-14 09:20:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+const csvFilePath='./csv/abc.csv';
+const csv=require('csvtojson');
+
+app.get('/getjson',(req,res)=>{
+    csv().fromFile(csvFilePath).then((jsonObj)=>{
+       res.send(jsonObj);
+     })
+})
+
+
+app.listen(process.env.PORT || 8083)
